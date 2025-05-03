1b8da06f844e9a31de1b14b2576d5b1163610e74
karstensensensen <simonblsoerensen@gmail.com>
2025-05-03 16:10:47 +0200

    Fix error on first run, add umath_subs function.

--- src/sympy_umath/sympy_umath.py
+++ src/sympy_umath/sympy_umath.py
@@ -1,7 +1,17 @@
 from sympy import Expr
-from sympy.utilities.lambdify import MODULES, MATH, MATH_DEFAULT, MATH_TRANSLATIONS, lambdify
+from sympy.utilities.lambdify import MODULES, MATH_TRANSLATIONS, lambdify
 
-MODULES['umath'] = (MATH.copy(), MATH_DEFAULT, MATH_TRANSLATIONS, ("from uncertainties.umath import *",))
+UMATH_DEFAULT = {}
+
+UMATH = UMATH_DEFAULT.copy()
+
+UMATH_TRANSLATIONS = MATH_TRANSLATIONS.copy()
+
+MODULES['umath'] = (UMATH, UMATH_DEFAULT, UMATH_TRANSLATIONS, ("from math import *", "from uncertainties.umath import *",))
 
 def lambdify_umath(args: list, expr: Expr, **kwargs):
     return lambdify(args, expr, modules='umath', **kwargs)
+
+def umath_subs(expr: Expr, subs: dict):
+    return lambdify_umath(list(subs.keys()), expr)(*subs.values())
+    
